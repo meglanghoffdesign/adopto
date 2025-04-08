f9e31fba2f3dad9c7b00fa672c7a945b1d7bfd73
Zach Donels <zachary.donels1@gmail.com>
2025-04-07 20:38:20 -0600

    fixed routing issue on homepage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,6 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
+
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/auth/LoginPage";
 import RegisterPage from "./pages/auth/RegisterPage";
@@ -6,7 +8,7 @@ import WelcomePage from "./pages/WelcomePage";
 import QuizPage from "./pages/QuizPage";
 import FavoritesPage from "./pages/FavoritesPage";
 import PetProfilePage from "./pages/PetProfilePage";
-import HomePage from "./pages/Homepage"; // Make sure this exists
+import HomePage from "./pages/Homepage";
 
 const App: React.FC = () => {
   return (
